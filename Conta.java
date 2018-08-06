709eeecfb3dc8958cb16b7ba62951b5b156ebc00
Guilherme Lima <33007095+guilsilva@users.noreply.github.com>
2018-08-06 15:46:56 -0300

    Composto

--- Conta.java
+++ Conta.java
@@ -1,8 +1,8 @@
 public class Conta {
 	private double saldo;
-	private int agencia;
-	private int numero;
-	private Cliente titular;
+	int agencia;
+	int numero;
+	Cliente titular;
 
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -27,29 +27,5 @@ public class Conta {
 	
 	public double getSaldo(){
 		return this.saldo;
-	}
-	
-	public int getNumero() {
-		return this.numero;
-	}
-	
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	
-	public int getAgencia() {
-		return this.agencia;
-	}
-	
-	public void setAgencia(int agencia) {
-		this.agencia = agencia;
-	}
-	
-	public void setTitular(Cliente titular) {
-		this.titular = titular;
-	}
-	
-	public Cliente getTitular() {
-		return titular;
 	}
 }
